1974b0225c206342b3716dce65bd9d570d964b63
arpit7759082 <arpit7759082266@gmail.com>
2025-11-08 11:33:39 +0530

    Add files via upload

---
+++ practical1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() {
+    // UsingSIMPLE STAR PATTERN
+
+    printf("PATTERN 1:\n");
+    printf("  *\n");
+    printf(" ***\n");
+    printf("*****\n");
+    printf("\n");
+// pattern 2: diamond shape
+printf("pattern 2:\n");
+printf("  *  \n");
+printf(" / \\ \n");
+printf("/   \\\n");
+printf("\\   /\n");
+printf(" \\ / \n");
+printf("  *     \n");
+printf("\n");
+// pattern 3: ARROW USING STAR AND BACKLASHES
+printf("pattern 3:\n");
+printf("   *   \n");
+printf("  /|\\  \n");
+printf(" / | \\ \n"); 
+printf("   |   \n");
+printf("   |   \n");
+printf("\n" );
+// pattern 4: SIMPLE HOUSE
+printf("pattern 4:\n");
+printf("  /\\  \n");
+printf(" /  \\ \n");
+printf("/____\\\n");
+printf("|    |\n");
+printf("|____|\n");
+return 0;
+}
